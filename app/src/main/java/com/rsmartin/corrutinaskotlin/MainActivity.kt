6da9333e853849d09d34eda4e800503a3c54768d
RafaelSMartin <rafels.martin.dev@gmail.com>
2023-06-12 16:24:26 +0200

    Dispatchers, Launch, Scope and Job

--- app/src/main/java/com/rsmartin/corrutinaskotlin/MainActivity.kt
+++ app/src/main/java/com/rsmartin/corrutinaskotlin/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.rsmartin.corrutinaskotlin
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
 import java.lang.StringBuilder
 import java.time.Instant
@@ -18,9 +23,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_coroutine_main)
         setUpView()
 
-        blokingExample()
-        suspendExample()
-        runBlockingExample()
+        //blokingExample()
+        //suspendExample()
+        //runBlockingExample()
+
+        //dispatcher()
+        //launch()
+        exampleJob()
     }
 
     private fun setUpView() {
@@ -80,5 +89,63 @@ class MainActivity : AppCompatActivity() {
         updateTextView("Tarea3 " + Thread.currentThread().name + Instant.now())
     }
 
+    /*
+      Dispatchers en que hilo se va a ejecutar una corrutina
+    */
+    fun dispatcher() {
+        runBlocking {
+            Log.d(TAG, "Hilo en el que se ejecuta 1: ${Thread.currentThread().name}")
+        }
+        runBlocking(Dispatchers.Unconfined) {//nos da igual
+            Log.d(TAG, "Hilo en el que se ejecuta 2: ${Thread.currentThread().name}")
+        }
+        runBlocking(Dispatchers.Default) { //uso intensivo de CPU
+            Log.d(TAG, "Hilo en el que se ejecuta 3: ${Thread.currentThread().name}")
+        }
+        runBlocking(Dispatchers.IO) { //operaciones entrada-salida de datos, webServices, BBDD
+            Log.d(TAG, "Hilo en el que se ejecuta 4: ${Thread.currentThread().name}")
+        }
+        runBlocking(newSingleThreadContext("MiHilo")) { //creando un nuevo hilo
+            Log.d(TAG, "Hilo en el que se ejecuta 5: ${Thread.currentThread().name}")
+        }
+        runBlocking(Dispatchers.Main) { //Hilo principal
+            Log.d(TAG, "Hilo en el que se ejecuta 6: ${Thread.currentThread().name}")
+        }
+    }
 
+    /*
+    Launch lanza una corrutina sin bloquear el hilo
+    Devuelve una referencia de la corrutina como Job
+    La corrutina es cancelada cuando el resultado del Job es cancelado
+    */
+    fun launch() {
+        Log.d(TAG, "launch")
+        Log.d(TAG, "Tarea1 " + Thread.currentThread().name + Instant.now())
+        GlobalScope.launch {
+            delay(timeMillis = 4000)
+            Log.d(TAG, "Tarea2 " + Thread.currentThread().name + Instant.now())
+        }
+        Log.d(TAG, "Tarea3 " + Thread.currentThread().name + Instant.now())
+    }
+
+    /*
+    Scope nos ayuda a definir el ciclo de vida de las corrutinas
+    Puede durar toda la app o estar vinculado a activity o viewmodel
+    Toda corrutina tiene q tener asociado un scope
+    GlobalScope esta asociado a la vida util de toda la app, no recomendable
+    Job elemento cancelable con un ciclo de vida cancelable a su finalizacion
+    Job no produce ningun valor como resultado, representa una pieza de trabajo q necesita ser realizada
+    Job se puede cancelar y finalizar su ejecucion
+    */
+
+    fun exampleJob() {
+        Log.d(TAG, "Job")
+        Log.d(TAG, "Tarea1 " + Thread.currentThread().name + Instant.now())
+        val job = GlobalScope.launch {
+            delay(timeMillis = 4000)
+            Log.d(TAG, "Tarea2 " + Thread.currentThread().name + Instant.now())
+        }
+        Log.d(TAG, "Tarea3 " + Thread.currentThread().name + Instant.now())
+        job.cancel() // no sale Tarea2 porque se cancela antes de terminar
+    }
 }
