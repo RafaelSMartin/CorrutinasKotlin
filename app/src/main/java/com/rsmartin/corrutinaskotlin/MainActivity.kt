9c86c06dc50b96bcb956115970d1d66a5fa22226
RafaelSMartin <rafels.martin.dev@gmail.com>
2023-06-12 15:06:46 +0200

    Update project Block, suspend and runBlocking

--- app/src/main/java/com/rsmartin/corrutinaskotlin/MainActivity.kt
+++ app/src/main/java/com/rsmartin/corrutinaskotlin/MainActivity.kt
@@ -1,30 +1,84 @@
 package com.rsmartin.corrutinaskotlin
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.lang.StringBuilder
+import java.time.Instant
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "CoroutinesTAG"
+    private lateinit var mText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coroutine_main)
+        setUpView()
 
-        //por defecto usa Dispatchers.Main
-        lifecycleScope.launch() {
-            val result = withContext(Dispatchers.IO) {
-                //mi llamada a bbdd o retrofit
-            }
-            updateTextView("MainActivity")
+        blokingExample()
+        suspendExample()
+        runBlockingExample()
+    }
+
+    private fun setUpView() {
+        mText = findViewById(R.id.mainTextView)
+    }
+
+    private fun updateTextView(message: String = "") {
+        mText.text = StringBuilder(mText.text.toString()).append(message).append("\n")
+    }
+
+    /*
+    Bloquea el hilo principal lo que tarde longTaskWithMessage
+     */
+    fun blokingExample() {
+        updateTextView("blokingExample")
+        updateTextView("Tarea1 " + Thread.currentThread().name + Instant.now())
+        longTaskWithMessage("Tarea2 ")
+        updateTextView("Tarea3 " + Thread.currentThread().name + Instant.now())
+    }
+
+    private fun longTaskWithMessage(message: String) {
+        Thread.sleep(4000)
+        updateTextView(message + Thread.currentThread().name + Instant.now())
+    }
+
+    /*
+    Suspend no nos va a bloquear el hilo principal, sirve para corrutinas que estaran en el centro de ella
+    Se pueden pausar y reanudar mas adelante,
+    Se puede ejecutar cualquier operacion de larga duracion sin bloquear
+    Necesitan una corrutina para ser lanzadas
+    *//*
+    RunBlocking nos permite ejecutar corrutina y bloquea hasta la finalizacion
+    Bloquea el hilo pero se ejecuta en orden
+    Se va a ejecutar una corrutina pero en el hilo en el q estamos bloqueando
+    */
+    fun suspendExample() {
+        updateTextView("suspendExample&runBlocking")
+        updateTextView("Tarea1 " + Thread.currentThread().name + Instant.now())
+        runBlocking {
+            delayCoroutine("Tarea2 ")
         }
+        updateTextView("Tarea3 " + Thread.currentThread().name + Instant.now())
     }
 
-    fun updateTextView(message: String) {
-        mainTextView.text = "desde la $message con corrutina IO"
+    private suspend fun delayCoroutine(message: String) {
+        delay(timeMillis = 4000)
+        updateTextView(message + Thread.currentThread().name + Instant.now())
     }
+
+    /*
+        RunBlocking se jecuta de otra manera en codigo
+    */
+    fun runBlockingExample() = runBlocking {
+        updateTextView("runBlocking")
+        updateTextView("Tarea1 " + Thread.currentThread().name + Instant.now())
+        delayCoroutine("Tarea2 ")
+        updateTextView("Tarea3 " + Thread.currentThread().name + Instant.now())
+    }
+
 
 }
--- app/src/main/java/com/rsmartin/corrutinaskotlin/MainViewModel.kt
+++ app/src/main/java/com/rsmartin/corrutinaskotlin/MainViewModel.kt
@@ -7,15 +7,4 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
-
-    fun doSomething() {
-        viewModelScope.launch {
-            val result = withContext(Dispatchers.IO) {
-                //mi llamada a bbdd o retrofit
-            }
-            //Hacer lo q quisieramos incluso actualizar la ui
-            //aqui ya estamos en el hilo principal de nuevo
-        }
-    }
-
 }
