eb60237b25516285cfd02dd0a8737657f451d834
RafaelSMartin <rafels.martin.dev@gmail.com>
2023-06-20 16:44:11 +0200

    Listas y secuencias, listas con corrutinas, primer flow

--- app/src/main/java/com/rsmartin/corrutinaskotlin/MainActivity.kt
+++ app/src/main/java/com/rsmartin/corrutinaskotlin/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.rsmartin.corrutinaskotlin
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import java.lang.StringBuilder
-import java.time.Instant
-import kotlin.system.measureTimeMillis
+import java.util.concurrent.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,17 +19,63 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine_main)
-        setUpView()
 
+        //show()
+        /*runBlocking {
+            runAsynchronous().forEach { i -> println(i) }
+        }*/
+
+        //Lanza una corrutina asincrona y luego llama al flow con collect
+        runBlocking {
+            launch {
+                for(j in 1..3) {
+                    println("No estoy bloqueado $j")
+                    delay(1000)
+                }
+            }
+            firstFlow().collect { value -> println(value) }
+        }
     }
 
-
-
-    private fun setUpView() {
-        mText = findViewById(R.id.mainTextView)
+    fun show() {
+        //listar().forEach { i->println(i) }
+        secuencia().forEach { i -> println(i) }
     }
 
-    private fun updateTextView(message: String = "") {
-        mText.text = StringBuilder(mText.text.toString()).append(message).append("\n")
+    /*
+    Listas y secuencias son sincronas y bloquean
+    */
+    fun listar(): List<Int> = listOf(3, 78, 98)
+
+    fun secuencia(): Sequence<Int> = sequence {
+        for (i in 1..3) {
+            Thread.sleep(1000)
+            yield(i)
+        }
+    }
+
+    /*
+    Listas con corrutinas de manera asincrona
+    */
+    suspend fun runAsynchronous(): List<Int> {
+        return runBlocking {
+            delay(1000)
+            return@runBlocking listOf(1, 2, 3)
+        }
+    }
+
+    /*
+    Flow es un call asincrono data string que se ejecuta de manera secuencial en la misma
+    corrutina y emite valores y finaliza normalmente con el estado completado o lanzando
+    una excepcion, se focaliza en un data string que emite valores secuencialmente.
+    Flow necesita siempre un constructor
+    */
+
+    // Emite un numero cada segundo
+    fun firstFlow(): Flow<Int> = flow {
+        for (i in 1..3) {
+            delay(1000)
+            emit(i)
+        }
     }
 }
